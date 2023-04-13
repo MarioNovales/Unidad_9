02b2dc1561f068f01c8632b3d3a10a89fd1d0ae9
Mario Novales <220004@fppiramide.com>
2023-04-13 10:33:08 +0200

    inicio

--- src/main/java/Ejercicio1.java
+++ src/main/java/Ejercicio1.java
@@ -9,33 +9,36 @@ public class Ejercicio1 {
             String consulta1 = "SELECT nombre_destino FROM destinos";
             String consulta2 = "SELECT COUNT(distinct nombre_cliente) AS 'cantidad' FROM clientes";
 
-            try {
-                Connection con = DriverManager.getConnection("jdbc:mysql://db-programacion-ej1.cbved7bhnvqz.us-east-1.rds.amazonaws.com:3306/viajes","admin","piramide");
-                Statement stmnt = con.createStatement();
-                ResultSet rs = stmnt.executeQuery(consulta);
+            try(Connection con = DriverManager.getConnection("jdbc:mysql://db-programacion-ej1.cbved7bhnvqz.us-east-1.rds.amazonaws.com:3306/viajes","admin","piramide")) {
 
-                while (rs.next()){
-                    String nombre = rs.getString("nombre_cliente");
-                    String apellido = rs.getString("apellido_cliente");
-                    System.out.printf("Nombre: %s, apellido: %s\n", nombre, apellido);
-                }
-                rs.close();
-                ResultSet rs1 = stmnt.executeQuery(consulta1);
-                while (rs1.next()){
-                    String nombre = rs1.getString("nombre_destino");
-                    System.out.printf("Nombre de destino: %s\n", nombre);
-                }
-                rs1.close();
+                try(Statement stmnt = con.createStatement()) {
+                    ResultSet rs = stmnt.executeQuery(consulta);
 
-                ResultSet rs2 = stmnt.executeQuery(consulta2);
-                while (rs2.next()){
-                    int cantidad = rs2.getInt("cantidad");
-                    System.out.printf("Cantidad de nombres de clientes distintos: %s\n", cantidad);
-                }
+                    while (rs.next()){
+                        String nombre = rs.getString("nombre_cliente");
+                        String apellido = rs.getString("apellido_cliente");
+                        System.out.printf("Nombre: %s, apellido: %s\n", nombre, apellido);
+                    }
 
-                rs2.close();
-                stmnt.close();
-                con.close();
+                    rs.close();
+                    rs = stmnt.executeQuery(consulta1);
+
+                    while (rs.next()){
+                        String nombre = rs.getString("nombre_destino");
+                        System.out.printf("Nombre de destino: %s\n", nombre);
+                    }
+
+                    rs.close();
+                    rs = stmnt.executeQuery(consulta2);
+
+                    while (rs.next()){
+                        int cantidad = rs.getInt("cantidad");
+                        System.out.printf("Cantidad de nombres de clientes distintos: %s\n", cantidad);
+                    }
+                    rs.close();
+                } catch (SQLException e){
+                    e.printStackTrace();
+                }
             } catch (SQLException e) {
                 e.printStackTrace();
             }
